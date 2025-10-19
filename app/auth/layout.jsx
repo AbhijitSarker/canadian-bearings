00229206c13bda7681d458cf2a8a783c84d799ac
AbhijitSarker <abhijit.srker@gmail.com>
2025-10-19 21:57:52 +0600

    refactor: remove TopHeader, Navbar, and Footer components from AuthLayout

--- app/auth/layout.jsx
+++ app/auth/layout.jsx
@@ -1,17 +1,9 @@
-"use client";
-import TopHeader from "@/components/common/topHeader";
-import Navbar from "@/components/common/navbar";
-import Footer from "@/components/home/footer";
-
 export default function AuthLayout({ children }) {
   return (
     <div>
-      <TopHeader />
-      <Navbar />
       <div className="container mx-auto py-12">
         {children}
       </div>
-      <Footer />
     </div>
   );
 }
